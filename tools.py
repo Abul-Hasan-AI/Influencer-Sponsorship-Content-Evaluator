542b724c7f1b1d2904789e5d3f99ede8e1d98b6f
Abul-Hasan-AI <61639562+Abul-Hasan-AI@users.noreply.github.com>
2024-12-11 15:37:47 +0000

    Formatting

--- tools.py
+++ tools.py
@@ -1,17 +1,21 @@
 import re
 
-def topic_guidelines_extractor(brief_file_content):
-    """ Function for extracting guidelines related to topic from brief"""    
 
-    # with open(brief_file_path, 'r',encoding='utf-8') as file: 
+def topic_guidelines_extractor(brief_file_content):
+    """Function for extracting guidelines related to topic from brief"""
+
+    # with open(brief_file_path, 'r',encoding='utf-8') as file:
     #     markdown_content = file.read()
 
-    pattern = re.compile(r"Step 1: Choose your video topic(.*?)## \*\*Step 2", re.DOTALL)
+    pattern = re.compile(
+        r"Step 1: Choose your video topic(.*?)## \*\*Step 2", re.DOTALL
+    )
     match = pattern.search(brief_file_content)
 
     if match:
         step_1_content = match.group(1).strip()
     return step_1_content
+
 
 def script_guidelines_extractor(brief_file_content):
 
